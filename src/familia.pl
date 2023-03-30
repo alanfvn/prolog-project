e820f8ff449ee66015b10cf73f6f8126285401ca
alanfvn <alandavidg16@gmail.com>
2023-03-30 16:53:06 -0600

    update knowledge base

--- src/familia.pl
+++ src/familia.pl
@@ -1,15 +1,16 @@
-hombre(jorge).
-hombre(miguel).
-hombre(pedro).
-mujer(maria).
-mujer(teresa).
-mujer(elena).
-mujer(raquel).
-progenitor(pedro,teresa).
-progenitor(maria,teresa).
-progenitor(maria,elena).
-progenitor(teresa,jorge).
-progenitor(teresa,raquel).
-progenitor(raquel,miguel).
-madre(X,W):- progenitor(X,W), mujer(X).
-padre(X,W):- progenitor(X,W), hombre(X).
+hombre(argelio).
+hombre(david).
+hombre(humberto).
+hombre(alan).
+mujer(florinda).
+mujer(ericka).
+mujer(olga).
+mujer(jackeline).
+progenitor(argelio, olga).
+progenitor(florinda, olga).
+progenitor(david, humberto).
+progenitor(ericka, humberto).
+progenitor(olga, alan).
+progenitor(humberto, alan).
+progenitor(olga, jackeline).
+progenitor(humberto, jackeline).
