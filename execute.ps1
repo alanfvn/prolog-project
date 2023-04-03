b0c3eb859b7a6c6cfef3db89d8332e59b5df1d00
alanfvn <alandavidg16@gmail.com>
2023-04-02 19:24:04 -0600

    Updated execute.ps1 script

--- execute.ps1
+++ execute.ps1
@@ -1,3 +1,14 @@
-./env/Scripts/activate
-cd ./src/
-py main.py
+$Folder = './env/'
+if (!(Test-Path -Path $Folder)){
+  py -m venv env
+  ./env/Scripts/activate
+  pip install -r ./requirements.txt
+  cd ./src/
+  py main.py
+
+} else {
+  # environment exists!
+  ./env/Scripts/activate
+   cd ./src/
+   py main.py
+}
